fdd2f2ac2f2c7a77f368a21d333a3106a2a52e9f
Nicole Chung <nicole.chung@gmail.com>
2020-02-04 08:35:08 -0500

    formatting

--- counter/svelte/src/App.svelte
+++ counter/svelte/src/App.svelte
@@ -1,43 +1,43 @@
 <script>
- let count = 0;
- function increment() {
-	 count += 1
- }
+  let count = 0;
+  function increment() {
+    count += 1;
+  }
 
- function decrement() {
-	 count -= 0
- }
+  function decrement() {
+    count -= 0;
+  }
 
- function reset() {
-	 count = 0
- }
+  function reset() {
+    count = 0;
+  }
 </script>
 
-<main>
-	<h1>Count {count}</h1>
-	<button on:click={increment}>+</button>
-	<button on:click={decrement}>-</button>
-	<button on:click={reset}>reset</button>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Count {count}</h1>
+  <button on:click={increment}>+</button>
+  <button on:click={decrement}>-</button>
+  <button on:click={reset}>reset</button>
+</main>
