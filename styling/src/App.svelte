10b0df76612398a1b1c664c89caeeccfad31aef6
Nicole Chung <nicole.chung@gmail.com>
2020-02-09 19:01:48 -0500

    adding a global style

---
+++ styling/src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+  export let name;
+</script>
+
+<style>
+  :global(body) {
+    /* this will apply to <body> */
+    margin: 0;
+    font-family: "Work Sans", sans-serif;
+  }
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+    font-family: "Pacifico", cursive;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
