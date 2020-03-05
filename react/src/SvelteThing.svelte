1e3b71886794dac1c94b3c93c501cb77b2ef1147
Nicole Chung <nicole.chung@gmail.com>
2020-03-04 22:32:35 -0500

    svelte colours

--- react/src/SvelteThing.svelte
+++ react/src/SvelteThing.svelte
@@ -2,12 +2,38 @@
   export let count;
 </script>
 
-<h1>Hello from Svelte!</h1>
-<button on:click>reset</button>
-<p>this is a prop from React: {count}</p>
-
 <style>
   h1 {
-    color: purple;
+    color: #424874;
+  }
+
+  .svelte-container {
+    background-color: #81f5ff;
+    padding: 20px;
+    margin-top: 30px;
+  }
+
+  button {
+    padding: 10px;
+    border-radius: 5px;
+    font-size: 12px;
+    border: none;
+    font-size: 20px;
+    background-color: #ffffdd;
+    text-transform: uppercase;
+    margin-bottom: 20px;
+  }
+
+  strong {
+    font-weight: 900;
   }
 </style>
+
+<div class="svelte-container">
+  <h1>Hello from Svelte!</h1>
+  <button on:click>reset</button>
+  <p>
+    this is a prop from React:
+    <strong>{count}</strong>
+  </p>
+</div>
--- react/src/main.js
+++ react/src/main.js
@@ -4,30 +4,54 @@ import { SvelteComponent } from "./SvelteComponent";
 import SvelteThing from "./SvelteThing.svelte";
 
 function App({ component }) {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const increment = () => {
     const newCount = count + 1;
-    setCount(newCount)
-  }
+    setCount(newCount);
+  };
   const decrement = () => {
     const newCount = count - 1;
-    setCount(newCount)
-  }
+    setCount(newCount);
+  };
   const reset = () => {
-    setCount(0)
-  }
+    setCount(0);
+  };
   return (
-    <div>
-      <p>This is a Svelte component inside a React app:</p>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
+    <div
+      style={{ backgroundColor: "#ffd5e5", padding: "20px", height: "100vh" }}
+    >
+      <h1>This is the react app:</h1>
+      <button
+        onClick={increment}
+        style={{
+          borderRadius: "5px",
+          padding: "10px",
+          fontSize: "20px",
+          marginRight: "20px",
+          border: "none",
+          backgroundColor: "#ffffdd"
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={decrement}
+        style={{
+          borderRadius: "5px",
+          padding: "10px",
+          fontSize: "20px",
+          border: "none",
+          backgroundColor: "#ffffdd"
+        }}
+      >
+        -
+      </button>
       <SvelteComponent component={component} count={count} onClick={reset} />
     </div>
   );
 }
 
 const main = document.querySelector("main");
-
 
 function update() {
   render(<App component={SvelteThing} />, main);
